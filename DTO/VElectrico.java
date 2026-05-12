dc52a7192a3f38081f41e2bbf3b55cca3c97e61b
MarinaDP2006 <marinadompas@gmail.com>
2026-05-12 18:07:53 +0200

    Update VElectrico.java

--- DTO/VElectrico.java
+++ DTO/VElectrico.java
@@ -1,15 +1,31 @@
 package DTO;
 
-public class VElectrico extends Vehiculo {
-	// Atributos
-	
-	
-	// Constructor
-	
-	
-	// Get y Set
-	
-	
-	// toString
-	
+public class VElectrico extends Vehiculo implements Alquilable {
+    private int autonomiaBateria;
+
+    public VElectrico(String matricula, String marca, String modelo, int añoFabricacion, String combustible, int plazas, double precioDia, String estado, int idCategoria, int autonomiaBateria) {
+        super(matricula, marca, modelo, añoFabricacion, combustible, plazas, precioDia, estado, idCategoria);
+        this.autonomiaBateria = autonomiaBateria;
+    }
+
+    public int getAutonomiaBateria() {
+        return autonomiaBateria;
+    }
+    public void setAutonomiaBateria(int autonomiaBateria) {
+        this.autonomiaBateria = autonomiaBateria;
+    }
+
+    // Método de la interfaz Alquilable
+    @Override
+    public double calcularPrecio(int dias) {
+        double precioBase = dias * getPrecioDia();
+        // Ejemplo: los eléctricos tienen un 5% de descuento
+        double descuento = precioBase * 0.05;
+        return precioBase - descuento;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Autonomía batería: " + autonomiaBateria + " km";
+    }
 }
