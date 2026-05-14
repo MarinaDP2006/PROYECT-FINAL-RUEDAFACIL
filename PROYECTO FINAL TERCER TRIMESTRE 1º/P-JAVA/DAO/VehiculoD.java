1ceb9d4e6a94e65a2257815e8be0a8a0db34a825
MarinaDP2006 <marinadompas@gmail.com>
2026-05-14 10:35:41 +0200

    Delete PROYECTO FINAL TERCER TRIMESTRE 1º directory

--- PROYECTO FINAL TERCER TRIMESTRE 1º/P-JAVA/DAO/VehiculoD.java
+++
@@ -1,8 +0,0 @@
-package DAO;
-
-public class VehiculoD {
-// insertar(VehiculoDTO vehiculo), actualizar(VehiculoDTO vehiculo), eliminar(String matricula)
-// buscarPorMatricula(String matricula) → devuelve VehiculoDTO
-// listarTodos() → ArrayList<VehiculoDTO>, listarPorEstado(String estado), listarPorCategoria(int idCategoria), actualizarEstado(String matricula, String nuevoEstado)
-
-}
