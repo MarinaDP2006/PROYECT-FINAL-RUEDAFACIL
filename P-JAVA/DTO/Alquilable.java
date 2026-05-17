a463f23f815c582f794d0907660302d44f9a5792
MarinaDP2006 <marinadompas@gmail.com>
2026-05-17 18:36:51 +0200

    Delete P-JAVA/DTO directory

--- P-JAVA/DTO/Alquilable.java
+++
@@ -1,6 +0,0 @@
-package DTO;
-
-	// Acción que hace el empleado para gestionar el alquiler 
-public interface Alquilable {
-    double calcularPrecio(int dias);
-}
--- P-JAVA/DTO/VehiculoNoDisponibleException.java
+++
@@ -1,7 +0,0 @@
-package DTO;
-
-public class VehiculoNoDisponibleException extends Exception {
-	public VehiculoNoDisponibleException(String message) {
-		super(message);
-	}
-}
