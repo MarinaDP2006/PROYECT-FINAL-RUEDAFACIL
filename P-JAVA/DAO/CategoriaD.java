27370dc3f30ee0231abf8aa4b391aab1eec04788
MarinaDP2006 <marinadompas@gmail.com>
2026-05-22 16:13:53 +0200

    Update CategoriaD.java

--- P-JAVA/DAO/CategoriaD.java
+++ P-JAVA/DAO/CategoriaD.java
@@ -1,18 +1,23 @@
 package DAO;
 import java.util.List;
-import DTO.Vehiculo;
-
-//PARA LOS METODOS INDICADOS SE USARÁN:
-//HashMap, TreeSet, ArrayList, LinkedList y uso de streams filter(), map(), sorted(), collect(), Comparable y Comparator e Iteradores para eliminación 
+import DTO.Categoria;
 
 public class CategoriaD {
 	// CRUD COMPLETO
 	public void crearCategoria(String categoria) {
-		
+		List<Categoria> categorias = new ArrayList<>(); // Lista de categorias
+		categorias.add(categorias); // Agregar a la lista
+		System.out.println("Categoria nueva: " + ());	
 	}
 
 	public void actualizarCategoria(String categoria) {
-		
+		List<Categoria> categorias = new ArrayList<>(); // Lista de categorias
+		for (Categoria cat : categoria) {
+			if () { // Se busca por Id y actualiza datos
+				System.out.println("Categoria con ID " +  " modificado.");
+				return;
+			}
+		}
 	}
 	
  	public void eliminarCategoria(String categoria) {
@@ -27,6 +32,6 @@ public class CategoriaD {
 	
 	// Listar todas las categorías
 	  	public void listarTodas() {
-	  	List<Vehiculo> categorias = List.of(); // Devuelve una lista de categorías
+	  	List<Categoria> categorias = List.of(); // Devuelve una lista de categorías
   	}
 }
