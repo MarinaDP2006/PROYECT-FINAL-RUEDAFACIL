f73a34cf9b16dfe901fce77b7d214fee3886799a
MarinaDP2006 <marinadompas@gmail.com>
2026-05-24 21:24:34 +0200

    Update Categoria.java

--- P-JAVA/DTO/Categoria.java
+++ P-JAVA/DTO/Categoria.java
@@ -24,12 +24,7 @@ public class Categoria {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
-    public String getDescripcion() {
-        return descripcion;
-    }
-    
-    
+            
     @Override
     public String toString() {
         return "Categoria " + idCategoria + "" +  "" + nombre;
