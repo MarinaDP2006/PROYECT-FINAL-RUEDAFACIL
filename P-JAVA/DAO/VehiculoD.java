e62d71aaacb96e7dc24779f6ce73bca04c2522f0
MarinaDP2006 <marinadompas@gmail.com>
2026-05-26 12:41:56 +0200

    Update VehiculoD.java

--- P-JAVA/DAO/VehiculoD.java
+++ P-JAVA/DAO/VehiculoD.java
@@ -1,22 +1,45 @@
 package DAO;
 import java.util.ArrayList;
-import DTO.Vehiculo;
+import java.util.Iterator;
+import java.util.List;
 import DTO.Categoria;
+import DTO.Vehiculo;
 
 public class VehiculoD {
+	
 // CRUD COMPLETO
-	public void crearVehiculo(Vehiculo vehiculo) {
-		
-	}
+    public void crearVehiculo(Vehiculo vehiculo) {
+        vehiculos.add(vehiculo);
+        System.out.println("Vehículo creado: " + vehiculo.getMatricula());
+    }
 	
-	public void actualizarVehiculo(Vehiculo vehiculo) {
-		
-	}
+    public void actualizarVehiculo(Vehiculo vehiculoActualizado) {
+        for (Vehiculo v : vehiculos) {
+            if (v.getMatricula().equals(vehiculoActualizado.getMatricula())) {
+                v.setModelo(vehiculoActualizado.getModelo());
+                v.setPrecioDia(vehiculoActualizado.getPrecioDia());
+                v.setDisponible(vehiculoActualizado.isDisponible());
+                v.setCategoria(vehiculoActualizado.getCategoria());
+                System.out.println("Vehículo actualizado: " + v.getMatricula());
+                return;
+            }
+        }
+        System.out.println("Vehículo no encontrado: " + vehiculoActualizado.getMatricula());
+    }
 	
-	public void eliminarVehiculo(Vehiculo vehiculo) {
-		
-	}
-		
+    public void eliminarVehiculo(Vehiculo vehiculo) {
+        Iterator<Vehiculo> it = vehiculos.iterator();
+        while (it.hasNext()) {
+            Vehiculo v = it.next();
+            if (v.getMatricula().equals(vehiculo.getMatricula())) {
+                it.remove();
+                System.out.println("Vehículo eliminado: " + vehiculo.getMatricula());
+                return;
+            }
+        }
+        System.out.println("Vehículo no encontrado: " + vehiculo.getMatricula());
+    }
+	
 // Buscar por matrícula
 	public static void buscarPorMatricula(String matricula) {
 		
@@ -28,8 +51,6 @@ public class VehiculoD {
 	}
 	
 // Listar  por categoría
-	public static void listarPorCategoria(Categoria categoria) {
-		ArrayList<Vehiculo> categorias = new ArrayList<Vehiculo>();
-		
-	}
+    public static void listarPorCategoria(int idCategoria) {
+    }
 }
