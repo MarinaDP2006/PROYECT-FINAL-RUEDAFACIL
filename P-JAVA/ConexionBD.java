2c05aa4f8aa057909a77ecc834fa3ce374ca4fe0
MarinaDP2006 <marinadompas@gmail.com>
2026-06-01 11:29:52 +0200

    Update ConexionBD.java

--- P-JAVA/ConexionBD.java
+++ P-JAVA/ConexionBD.java
@@ -1,7 +1,10 @@
+package src;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class Conexion {
+public class ConexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/ruedafacil";
     private static final String USER = "root";
     private static final String PASS = "";
@@ -9,7 +12,7 @@ public class Conexion {
     public static Connection conectar() {
         try {
             return DriverManager.getConnection(URL, USER, PASS);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println("Error al conectar: " + e.getMessage());
             return null;
         }
