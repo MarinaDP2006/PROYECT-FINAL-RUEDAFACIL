332254a71d996c42a3a61b388330a1c12cad75e4
MarinaDP2006 <marinadompas@gmail.com>
2026-06-01 11:54:14 +0200

    Update Vehiculo.java

--- P-JAVA/DTO/Vehiculo.java
+++ P-JAVA/DTO/Vehiculo.java
@@ -10,8 +10,8 @@ public class Vehiculo {
 	private double precioDia;
 	private String estado;
 	private String categoria;
-	
-	public Vehiculo(String matricula, String marca, String modelo, int añoFabricacion, String combustible, int plazas, double precioDia, String estado, String categoria) {
+
+	public Vehiculo(String matricula, String marca, String modelo, int añoFabricacion, String combustible, int plazas,	double precioDia, String estado, String categoria) {
 		this.matricula = matricula;
 		this.marca = marca;
 		this.modelo = modelo;
@@ -23,66 +23,84 @@ public class Vehiculo {
 		this.categoria = categoria;
 	}
 
+	// Constructor para crear nuevos vehiculos
 	public Vehiculo() {
 	}
 
 	public String getMatricula() {
 		return matricula;
 	}
+
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public String getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+
 	public int getAñoFabricacion() {
 		return añoFabricacion;
 	}
+
 	public void setAñoFabricacion(int añoFabricacion) {
 		this.añoFabricacion = añoFabricacion;
 	}
+
 	public String getCombustible() {
 		return combustible;
 	}
+
 	public void setCombustible(String combustible) {
 		this.combustible = combustible;
 	}
+
 	public int getPlazas() {
 		return plazas;
 	}
+
 	public void setPlazas(int plazas) {
 		this.plazas = plazas;
 	}
+
 	public double getPrecioDia() {
 		return precioDia;
 	}
+
 	public void setPrecioDia(double precioDia) {
 		this.precioDia = precioDia;
 	}
+
 	public String getEstado() {
 		return estado;
 	}
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
 	public String getCategoria() {
 		return categoria;
 	}
+
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
-		
+
 	public String toString() {
-		return "Vehiculo: " + matricula + marca + "Modelo - " + modelo + "Fabricación en " + añoFabricacion + " Combustible - " + combustible + " Plazas - " + plazas + " Precio x día - " + precioDia
+		return "Vehiculo: " + matricula + marca + "Modelo - " + modelo + "Fabricación en " + añoFabricacion	+ " Combustible - " + combustible + " Plazas - " + plazas + " Precio x día - " + precioDia
 				+ " Estado - " + estado + " Categoria -" + categoria;
 	}
 }
